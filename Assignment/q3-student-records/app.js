e29341097c1f1ecb8cbb21f833012c4e3e5444d3
Jozza Batool <2500594@students.au.edu.pk>
2026-09-26 17:41:40 +0500

    Update app.js

--- Assignment/q3-student-records/app.js
+++ Assignment/q3-student-records/app.js
@@ -1,32 +1,12 @@
+//jozza
 const students = [
-    {
-        name: "Jozza",
-        rollNo: "CS-594",
-        marks: 88
-    },
-    {
-        name: "Abbas",
-        rollNo: "CS-601",
-        marks: 74
-    },
-    {
-        name: "Ali",
-        rollNo: "CS-602",
-        marks: 91
-    },
-    {
-        name: "Sara",
-        rollNo: "CS-603",
-        marks: 65
-    },
-    {
-        name: "Hussain",
-        rollNo: "CS-604",
-        marks: 48
-    }
+    { name: "Jozza", rollNo: "CS-594", marks: 88 },
+    { name: "Abass", rollNo: "CS-601", marks: 74 },
+    { name: "Ali", rollNo: "CS-602", marks: 91 },
+    { name: "Sara", rollNo: "CS-603", marks: 65 },
+    { name: "Hussain", rollNo: "CS-604", marks: 48 }
 ];
-
-// Return the grade according to marks
+// Return grade according to marks
 function getGrade(marks) {
 
     if (marks >= 85) {
@@ -42,39 +22,27 @@ function getGrade(marks) {
         return "F";
     }
 }
+const table = document.getElementById("studentTable");
 
-const studentTable = document.getElementById("studentTable");
-const averageElement = document.getElementById("average");
-const topScorerElement = document.getElementById("topScorer");
-
-let totalMarks = 0;
+let total = 0;
 let topStudent = students[0];
-
 for (let i = 0; i < students.length; i++) {
 
-    const student = students[i];
-
-    const row = document.createElement("tr");
-
-    row.innerHTML = `
-        <td>${student.name}</td>
-        <td>${student.rollNo}</td>
-        <td>${student.marks}</td>
-        <td>${getGrade(student.marks)}</td>
+    table.innerHTML += `
+        <tr>
+            <td>${students[i].name}</td>
+            <td>${students[i].rollNo}</td>
+            <td>${students[i].marks}</td>
+            <td>${getGrade(students[i].marks)}</td>
+        </tr>
     `;
-
-    studentTable.appendChild(row);
-
-    totalMarks = totalMarks + student.marks;
-
-    if (student.marks > topStudent.marks) {
-        topStudent = student;
-    }
+    total = total + students[i].marks;
+    if (students[i].marks > topStudent.marks) {
+        topStudent = students[i];  }
 }
+const average = total / students.length;
+document.getElementById("average").textContent =
+    "Class Average: " + average.toFixed(1);
 
-const average = totalMarks / students.length;
-
-averageElement.textContent = "Class Average: " + average.toFixed(1);
-
-topScorerElement.textContent =
-    "Top Scorer: " + topStudent.name + " (" + topStudent.marks + " marks)";
+document.getElementById("topScorer").textContent =
+    "Top Scorer: " + topStudent.name;
